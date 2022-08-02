046bcddf8da9764cc16a59679875c14f9ae66961
Manunchaya Chongsutakawewong <sung478@hotmail.com>
2022-08-03 00:52:34 +0700

    edit-navBar-and-mobileResponsive

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -15,12 +15,12 @@ const motionList = {
 export function NavBar () {
     return(
         <motion.div initial={{ opacity: 0.1 }} animate={{ opacity: 1 }} transition={{ duration: 2 }} className="navBar">
-            <motion.div initial={{ scale: 5, x: "43vw", y: "50vh" }} animate={{ scale: 1 , x: 0, y: 0 }}  transition={{ duration: 1, ease: "easeIn", delay: 2}} id="icon">
+            <motion.div initial={{ scale: 4, x: "39vw", y: "50vh" }} animate={{ scale: 1 , x: 0, y: 0 }}  transition={{ duration: 1, ease: "easeIn", delay: 2}} id="icon">
                 <motion.img src={navLogo} initial={{ rotate: 0 }} animate={{ rotate: 360 }} transition={{duration: 2, delay:0.5}} whileHover={{ borderRadius: "20%" }} />
-                <motion.h3 initial={{ scale: 2 }} animate={{ scale: 1 }} transition={{ type: "spring" }}>Sung</motion.h3>
+                <motion.h3 initial={{ scale: 2 }} animate={{ scale: 1 }} transition={{ type: "spring" }}><motion.a variants={motionList} whileHover="hoverMotion" href="#home" >Sung</motion.a></motion.h3>
             </motion.div>
             <div id="navBar-link">
-                <motion.a variants={motionList} whileHover="hoverMotion" href="#home"><p>Home</p></motion.a>
+                {/* <motion.a variants={motionList} whileHover="hoverMotion" href="#home"><p>Home</p></motion.a> */}
                 <motion.a variants={motionList} whileHover="hoverMotion" href="#aboutMe"><p>About Me</p></motion.a>
                 <motion.a variants={motionList} whileHover="hoverMotion" href="#experiences"><p>Experiences</p></motion.a>
                 <motion.a variants={motionList} whileHover="hoverMotion" href="#skills"><p>Skills</p></motion.a>
