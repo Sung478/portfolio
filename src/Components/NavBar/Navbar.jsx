5dc48dac2b6c1a2ed92b1cce222f82131f170c05
Manunchaya Chongsutakawewong <sung478@hotmail.com>
2022-08-03 18:17:03 +0700

    edit-icon-layout

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -15,7 +15,7 @@ const motionList = {
 export function NavBar () {
     return(
         <motion.div initial={{ opacity: 0.1 }} animate={{ opacity: 1 }} transition={{ duration: 2 }} className="navBar">
-            <motion.div initial={{ scale: 4, x: "39vw", y: "50vh" }} animate={{ scale: 1 , x: 0, y: 0 }}  transition={{ duration: 1, ease: "easeIn", delay: 2}} id="icon">
+            <motion.div initial={{ scale: 1, x: "18vw", y: "50vh" }} animate={{ scale: 0.3 , x: -80, y: 0, margin: 0, padding: 0 }}  transition={{ duration: 1, ease: "easeIn", delay: 2}} id="icon">
                 <motion.img src={navLogo} initial={{ rotate: 0 }} animate={{ rotate: 360 }} transition={{duration: 2, delay:0.5}} whileHover={{ borderRadius: "20%" }} />
                 <motion.h3 initial={{ scale: 2 }} animate={{ scale: 1 }} transition={{ type: "spring" }}><motion.a variants={motionList} whileHover="hoverMotion" href="#home" >Sung</motion.a></motion.h3>
             </motion.div>
